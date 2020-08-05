c07cfdd4a20f1c2fa37dcab13561e04ed316050b
HarshithaBoppudi <67325628+HarshithaBoppudi@users.noreply.github.com>
2020-08-05 20:29:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+//Create variables here
+var dog,dog1;
+var dogHappy;
+var database;
+var foodS,foodStock;
+function preload()
+{
+  //load images here
+  dog1.loadImage("images/img.png")
+}
+
+function setup() {
+  createCanvas(500, 500);
+  database=firebase.database();
+  dog=createSprite(250,300,40,40);
+  dog.addImage(dog1);
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+}
+
+
+function draw() {  
+  background(46,139,85)
+if(keyWentDown(UP_ARROW)){
+  writeStock(foodS)
+}
+  drawSprites();
+  //add styles here
+  textSize(30);
+  text("foodStock:"+foodStock,200,60)
+
+}
+function readStock(data){
+  foodS=data.val
+}
+function writeStock(x){
+  if(x<=0){
+    x-=0;
+  }
+  else{
+    x=x-1;
+  }
+  database.ref('/').update({
+    food:x
+  })
+}
+
+
