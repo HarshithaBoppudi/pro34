b5347802f914b959c14c568133a9b94ba0456038
HarshithaBoppudi <67325628+HarshithaBoppudi@users.noreply.github.com>
2020-08-11 19:07:23 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function preload()
 {
   //load images here
   dog1=loadImage("images/dogImg.png");
-  dogHappy=loadImage("images/dogImg.png");
+  dogHappy=loadImage("images/dogImg1.png");
 }
 
 function setup() {
@@ -15,7 +15,7 @@ function setup() {
   database=firebase.database();
   dog=createSprite(250,300,40,40);
   dog.addImage(dog1);
-
+  dog.scale=0.2;
   foodStock=database.ref('Food');
   foodStock.on("value",readStock);
 }
@@ -25,28 +25,29 @@ function draw() {
   background(46,139,85)
 if(keyWentDown(UP_ARROW)){
   writeStock(foodS);
-  dog.addImage(dogImage);
+  dog.addImage(dogHappy);
 }
   drawSprites();
   //add styles here
+  fill("red");
   textSize(30);
-  text("foodStock:"+foodStock,200,60)
+  text("foodStock:"+foodS,200,60)
 
 }
 function readStock(data){
-  foodS=data.val
+  foodS=data.val();
 }
 function writeStock(x){
-  database.ref('/').update({
-    food:x
-  })
+  
   if(x<=0){
-    x-=0;
+    x=0;
   }
   else{
     x=x-1;
   }
- 
+  database.ref('/').update({
+    Food:x
+  })
 }
 
 
